6bc1ec1e87831e17689e466f8417165abaa1e62c
tdavis0828 <davis.t828@gmail.com>
2022-08-30 18:46:16 -0700

    pushing for linter troubleshooting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import logo from '../img/logo2.png';
 
 const StyledNav = styled.nav`
@@ -17,6 +18,18 @@ function Navbar() {
   return (
     <StyledNav>
       <img src={logo} alt="company logo" />
+      <div className="links">
+        <NavLink to="/">Download</NavLink>
+        <NavLink to="/">Safety</NavLink>
+        <NavLink to="/">About Us</NavLink>
+      </div>
+      <div className="search-bar">
+        <p>Search Bar</p>
+      </div>
+      <div className="buttons">
+        <NavLink to="/">Toggle switch</NavLink>
+        <NavLink to="/">Notification Icon</NavLink>
+      </div>
     </StyledNav>
   );
 }
