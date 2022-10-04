35ac77cc4d7e3141095a7ce12e674c23245d603e
tdavis0828 <davis.t828@gmail.com>
2022-10-04 16:03:12 -0700

    adding user authenitcation

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,10 @@ const multer = require('multer');
 const AWS = require('aws-sdk');
 let User = require('../schemas/userSchema');
 require('dotenv/config');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+router.use(cors());
 
 // creating the storage variable to upload the file and providing the destination folder,
 // if nothing is provided in the callback it will get uploaded in main directory
@@ -14,14 +18,10 @@ const storage = multer.memoryStorage({
 
 // below variable is define to check the type of file which is uploaded
 const fileFilter = (req, file, cb) => {
-  if (
-    fileFilter.mimetype === 'image/jpeg' ||
-    fileFilter.mimetype === 'image/jpg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+  const isValidType = ['image/jpeg', 'image/jpg', 'image/png'].includes(
+    file.mimetype
+  );
+  cb(null, isValidType);
 };
 
 // defining the upload variable for the configuration of photo being uploaded
@@ -30,7 +30,7 @@ const upload = multer({ storage: storage, fileFilter: fileFilter });
 // Now creating the S3 instance which will be used in uploading photo to s3 bucket.
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRECT_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 /* GET Routes */
@@ -50,32 +50,17 @@ router.route('/:id').get((req, res) => {
 /* POST Routes*/
 // Route for adding a new user
 router.post('/addUser', upload.single('images'), async (req, res) => {
+  // const encrypedPassword = await bcrypt.hash(password, 10);
+  const currentUser = User.findOne({ email });
+  if (currentUser) {
+    res.send({ error: 'User already exists' });
+  }
   try {
-    console.log(req.body);
-    const {
-      userId,
-      firstName,
-      lastName,
-      gender,
-      preference,
-      username,
-      email,
-      password,
-      birthday,
-      smoker,
-      drinker,
-      interests,
-      isLoggedIn,
-    } = req.body;
-    console.log('req.file: ', req.file);
-    console.log('req.files: ', req.files);
-
     const params = {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: req.body.originalname,
-      Body: req.file.buffer,
-      ACL: 'public-read-write',
-      ContentType: 'image/jpeg',
+      Bucket: process.env.AWS_BUCKET_NAME, // bucket that we made earlier
+      Key: `${Date.now()}-${req.file.originalname}`, // Name of the image
+      Body: req.file.buffer, // Body which will contain the image in buffer format
+      ContentType: 'image/jpeg', // Necessary to define the image content-type to view the photo in the browser with the link
     };
     s3.upload(params, async (error, data) => {
       if (error) {
@@ -83,28 +68,36 @@ router.post('/addUser', upload.single('images'), async (req, res) => {
         return res.status(500).send({ err: error });
       }
       const newUser = new User({
-        userId,
-        firstName,
-        lastName,
+        ...req.body,
         images: data.Location,
-        gender,
-        preference,
-        username,
-        email,
-        password,
-        birthday,
-        smoker,
-        drinker,
-        interests,
-        isLoggedIn,
       });
+      console.log('s3 data: ', data);
       await newUser.save();
-      res.json('New user added!');
-      console.log(data);
+      res.json('New user added');
     });
   } catch (err) {
     console.log(err.message);
     res.status(400).json(err.message);
+  }
+});
+
+//Route for logging user in
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  console.log(user);
+  if (user) {
+    const token = jwt.sign(
+      {
+        username: user.username,
+      },
+      'secret123'
+    );
+    return res.json({ status: 'ok', user: true, data: token });
+  } else {
+    return res.json({ status: 'User not found', user: false });
   }
 });
 
